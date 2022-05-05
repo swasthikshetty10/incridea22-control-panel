d682f6dd03a3dc3eb9343d06e7f35f6713ee9357
swasthik_shetty <swasthikshetty10902@gmail.com>
2022-05-05 11:47:23 +0700

    Cp

---
+++ src/Compnents/ControlPanel/Users.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Users({ participants }) {
+    return (
+        <div className='h-full text-center  border-opacity-40 border-gray-300 border-r-2 '>
+            <h2 className='font-semibold border-b-2 p-2 border-opacity-40 border-gray-300  sm:p-4 text-2xl'>Participants</h2>
+            <div className='py-3'>
+                <div className='h-[77vh] tablescroll  overflow-y-scroll w-full'>
+                    {
+                        participants.map((obj, index) =>
+                            <div key={index} className="bg-gray-700 hover:bg-opacity-50 cursor-pointer transform ease-in-out duration-50 bg-opacity-90 m-2 mx-6 px-2 py-1 flex justify-between ">
+                                <div className='flex-col gap-3  w-full '>
+                                    {obj.pIds.map((pid, index) => <div key={index} className='bg-gray-500 p-2 bg-opacity-20 inline-flex flex-nowrap my-1 justify-between w-full '>
+                                        {pid}
+                                    </div>
+                                    )}
+                                </div>
+                            </div>)}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Users
---
+++ src/Compnents/ControlPanel/index.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { auth, getEvent, getOrganiser } from '../../firebaseConfig'
+import Users from './Users'
+import SearchBar from './Utility/SearchBar'
+import {
+    collection, onSnapshot
+} from 'firebase/firestore'
+import { db } from '../../firebaseConfig'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AuthContext } from '../../Context/AuthContext'
+function Dashboard(props) {
+    const [query, setQuery] = useState("");
+    const eventName = "capture the flag"
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const userCtx = useContext(AuthContext)
+    const navigator = useNavigate()
+    const { id } = useParams()
+    console.log(userCtx)
+    useEffect(() => {
+        const colRef = collection(db, "Events")
+        //real time update
+        if (userCtx) {
+            onSnapshot(colRef, (snapshot) => {
+                snapshot.docs.forEach((doc) => {
+                    if (doc.id === id) {
+                        console.log(doc.id)
+                        setData(doc.data())
+                        setLoading(false)
+                        return
+                    }
+                })
+            }
+            )
+
+        }
+        else {
+            navigator("/")
+        }
+
+    }, [])
+    return (<>
+        {loading ? <div>Loading</div> :
+
+            <div className="w-full px-5 relative  text-white p-5 bg-gray-900">
+                <div className='flex  justify-between items-center '>
+                    <h1 className='text-4xl py-5'>Event XYZ</h1>
+                    <SearchBar query={query} setQuery={setQuery} />
+                    <button onClick={() => auth.signOut()}>Sign out</button>
+                </div>
+                <div className='flex justify-between 
+             border-2 border-opacity-40 border-gray-300 overflow-hidden shadow-md  shadow-gray-800'>
+                    <Users participants={data.participants} />
+                    {/* <Rounds participants={data.participants} />
+                    <Winners participants={data.participants} /> */}
+                </div>
+
+            </div>
+
+        }
+
+    </>
+    )
+}
+
+export default Dashboard
